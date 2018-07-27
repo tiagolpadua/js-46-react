515e09f71e3cb8d677a17c020bbe9a6c2b889926
Tiago Pádua <tiagolpadua@gmail.com>
2018-07-27 17:17:20 -0300

    cap 6

--- twitelum/src/App.js
+++ twitelum/src/App.js
@@ -7,6 +7,13 @@ import TrendsArea from './components/TrendsArea';
 import Tweet from './components/Tweet';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      novoTweet: ''
+    };
+  }
+
   render() {
     return (
       <Fragment>
@@ -18,13 +25,28 @@ class App extends Component {
             <Widget>
               <form className="novoTweet">
                 <div className="novoTweet__editorArea">
-                  <span className="novoTweet__status">0/140</span>
+                  <span
+                    className={`
+																	novoTweet__status
+																	${this.state.novoTweet.length > 140 ? 'novoTweet__status--invalido' : ''}
+													`}
+                  >
+                    {this.state.novoTweet.length}/140
+                  </span>
                   <textarea
                     className="novoTweet__editor"
-                    placeholder="O que está acontecendo?"
+                    value={this.state.novoTweet}
+                    onInput={event =>
+                      this.setState({ novoTweet: event.target.value })
+                    }
+                    placeholder="O	que	está	acontecendo?"
                   />
                 </div>
-                <button type="submit" className="novoTweet__envia">
+                <button
+                  className="novoTweet__envia"
+                  disabled={this.state.novoTweet.length > 140 ? true : false}
+                  type="submit"
+                >
                   Tweetar
                 </button>
               </form>
