91e0704c0eee8ef9983beb568b34d7f35bf82992
Tiago Pádua <tiagolpadua@gmail.com>
2018-07-27 16:40:41 -0300

    cap 3

--- twitelum/src/components/Cabecalho/index.js
+++ twitelum/src/components/Cabecalho/index.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 
 class Cabecalho extends Component {
-    render() {
-        return (
-            <header className="cabecalho">
-                <div className="cabecalho__container container">
-                    <h1 className="cabecalho__logo">
-                        <a href="">Twitelum</a>
-                    </h1>
-                    <nav className="navMenu">
-                        <ul className="navMenu__lista">
-                            <li className="navMenu__item">
-                                <a className="navMenu__link">
-                                    Bem vindo(a): <br />
-                                    <strong>@fulanodetal</strong>
-                                </a>
-                            </li>
-                            <li className="navMenu__item">
-                                <a className="navMenu__link" href="">Página Inicial</a>
-                            </li>
-                            <li className="navMenu__item">
-                                <a className="navMenu__link">Hashtags</a>
-                            </li>
-                            <li className="navMenu__item">
-                                <a className="navMenu__link">Logout</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header className="cabecalho">
+        <div className="cabecalho__container container">
+          <h1 className="cabecalho__logo">
+            <a href="">Twitelum</a>
+          </h1>
+          <nav className="navMenu">
+            <ul className="navMenu__lista">
+              <li className="navMenu__item">
+                <a className="navMenu__link">
+                  Bem vindo(a): <br />
+                  <strong>@fulanodetal</strong>
+                </a>
+              </li>
+              <li className="navMenu__item">
+                <a className="navMenu__link" href="">
+                  Página Inicial
+                </a>
+              </li>
+              <li className="navMenu__item">
+                <a className="navMenu__link">Hashtags</a>
+              </li>
+              <li className="navMenu__item">
+                <a className="navMenu__link">Logout</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Cabecalho;
