e5b17c7ef5a4fd83ea2010fccd336566ed643f2d
Tiago Pádua <tiagolpadua@gmail.com>
2018-07-27 17:00:34 -0300

    cap 4

--- twitelum/src/App.js
+++ twitelum/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import Cabecalho from './components/Cabecalho';
+import NavMenu from './components/NavMenu';
 import './App.css';
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Cabecalho usuario="@omariosouto" />
+        <Cabecalho>
+          <NavMenu usuario="@omariosouto" />
+        </Cabecalho>
       </Fragment>
     );
   }
