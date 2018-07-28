1035b41411f55d7866e63c8275f3cc3b69777945
Tiago Pádua <tiagolpadua@gmail.com>
2018-07-28 08:28:29 -0300

    cap 10

--- twitelum/src/routes.js
+++ twitelum/src/routes.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+
 // Páginas
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
+
 const Roteamento = () => {
   return (
     <Switch>
-      <Route path="/" component={Home} exact />
       <Route path="/login" component={LoginPage} />
+      <PrivateRoute path="/" component={Home} exact />
     </Switch>
   );
 };
+
+class PrivateRoute extends React.Component {
+  render() {
+    const Component = this.props.component;
+    const props = this.props;
+    if (estaAutenticado()) {
+      return <Route render={() => <Component {...props} />} />;
+    } else {
+      return <Redirect to="/login" />;
+    }
+  }
+}
+
+function estaAutenticado() {
+  if (localStorage.getItem('TOKEN')) {
+    return true;
+  } else {
+    return false;
+  }
+}
 export default Roteamento;
