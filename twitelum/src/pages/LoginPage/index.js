1f2ed63e7a32f7def4c69b49e6d5facc015d588f
Tiago Pádua <tiagolpadua@gmail.com>
2018-07-28 08:11:36 -0300

    cap 8

--- twitelum/src/pages/LoginPage/index.js
+++ twitelum/src/pages/LoginPage/index.js
@@ -4,18 +4,61 @@ import Widget from '../../components/Widget';
 import './loginPage.css';
 
 class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      errorMessage: ''
+    };
+  }
+
+  fazerLogin = event => {
+    event.preventDefault();
+    // console.log(this.inputLogin.value)
+    // console.log(this.inputSenha.value)
+    const dadosDeLogin = {
+      login: this.inputLogin.value,
+      senha: this.inputSenha.value
+    };
+    fetch('http://twitelum-api.herokuapp.com/login', {
+      method: 'POST',
+      body: JSON.stringify(dadosDeLogin)
+    })
+      // .then(dados => console.log(dados))
+      .then(response => {
+        if (!response.ok) throw response;
+        return response.json();
+      })
+      .then(responseEmJSON => {
+        localStorage.setItem('TOKEN', responseEmJSON.token);
+        this.props.history.push('/');
+      })
+      .catch(responseError => {
+        responseError.json().then(response => {
+          console.log(response);
+          this.setState({
+            errorMessage: response.message
+          });
+        });
+      });
+  };
+
   render() {
     return (
       <div className="loginPage">
         <div className="container">
           <Widget>
             <h1 className="loginPage__title">Twitelum</h1>
-            <form className="loginPage__form" action="/">
+            <form
+              className="loginPage__form"
+              action="/"
+              onSubmit={this.fazerLogin}
+            >
               <div className="loginPage__inputWrap">
                 <label className="loginPage__label" htmlFor="login">
                   Login
                 </label>
                 <input
+                  ref={inputLogin => (this.inputLogin = inputLogin)}
                   className="loginPage__input"
                   type="text"
                   id="login"
@@ -27,15 +70,20 @@ class LoginPage extends Component {
                   Senha
                 </label>
                 <input
+                  ref={inputSenha => (this.inputSenha = inputSenha)}
                   className="loginPage__input"
                   type="password"
                   id="senha"
                   name="senha"
                 />
               </div>
-              {/* <div className="loginPage__errorBox">
-                                Mensagem de erro!
-                            </div> */}
+              {this.state.errorMessage ? (
+                <div className="loginPage__errorBox">
+                  {this.state.errorMessage}
+                </div>
+              ) : (
+                ''
+              )}
               <div className="loginPage__inputWrap">
                 <button className="loginPage__btnLogin" type="submit">
                   Logar
